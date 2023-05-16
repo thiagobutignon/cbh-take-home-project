50ecb80d5a397bad2c5378e040b9971d8b5015d9
Thiago Butignon <thiagobutignon@gmail.com>
2023-05-16 16:01:34 -0300

    test: Should return the hash of the event as partition

--- dpk.test.js
+++ dpk.test.js
@@ -1,27 +1,6 @@
 const { deterministicPartitionKey } = require("./dpk");
 const crypto = require("crypto");
 
-jest.mock("crypto", () => ({
-  createHash: jest.fn().mockReturnThis(),
-  update: jest.fn().mockReturnThis(),
-  digest: jest.fn(),
-}));
-
-const mockIsString = jest.fn();
-const mockStringify = jest.fn();
-const mockCreateHash = jest.fn();
-
-beforeEach(() => {
-  crypto.createHash.mockClear();
-  crypto.update.mockClear();
-  crypto.digest.mockClear();
-
-  mockIsString.mockClear();
-  mockStringify.mockClear();
-  mockCreateHash.mockClear();
-});
-
-
 describe("deterministicPartitionKey", () => {
   test("Returns the literal '0' when given no input", () => {
         const sut = deterministicPartitionKey();
@@ -31,7 +10,7 @@ describe("deterministicPartitionKey", () => {
   test("Should return trivial partition key when event has no partitionKey", () => {
     const event = {};
     const sut = deterministicPartitionKey(event);
-    expect(sut).toEqual("0");
+    expect(sut).toEqual("c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862");
   });
 
 
@@ -39,5 +18,15 @@ describe("deterministicPartitionKey", () => {
     const mockEvent = { partitionKey: "abc" };
     const sut = deterministicPartitionKey(mockEvent);
     expect(sut).toBe("abc");
+  });
+
+  test("Should return the hash of the event as the partition key when event has no partitionKey", () => {
+    const event = { key: "value" };
+    const result = deterministicPartitionKey(event);
+    const expectedHash = crypto
+      .createHash("sha3-512")
+      .update(JSON.stringify(event))
+      .digest("hex");
+    expect(result).toEqual(expectedHash);
   });
 });
