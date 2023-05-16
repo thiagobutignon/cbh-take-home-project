ff14e764ff92fcfd824221ba9c3acfa0144e960c
Thiago Butignon <thiagobutignon@gmail.com>
2023-05-16 15:58:50 -0300

    Should return trivial partition key when has no partition Key

--- dpk.test.js
+++ dpk.test.js
@@ -1,26 +1,43 @@
-const { deterministicPartitionKey,
-  isString,
-  stringify,
-  createHash } = require("./dpk");
+const { deterministicPartitionKey } = require("./dpk");
 const crypto = require("crypto");
 
-// describe("deterministicPartitionKey", () => {
-//   it("Returns the literal '0' when given no input", () => {
-//     const trivialKey = deterministicPartitionKey();
-//     expect(trivialKey).toBe("0");
-//   });
-// });
+jest.mock("crypto", () => ({
+  createHash: jest.fn().mockReturnThis(),
+  update: jest.fn().mockReturnThis(),
+  digest: jest.fn(),
+}));
+
+const mockIsString = jest.fn();
+const mockStringify = jest.fn();
+const mockCreateHash = jest.fn();
+
+beforeEach(() => {
+  crypto.createHash.mockClear();
+  crypto.update.mockClear();
+  crypto.digest.mockClear();
+
+  mockIsString.mockClear();
+  mockStringify.mockClear();
+  mockCreateHash.mockClear();
+});
+
 
 describe("deterministicPartitionKey", () => {
   test("Returns the literal '0' when given no input", () => {
-        const trivialKey = deterministicPartitionKey();
-        expect(trivialKey).toBe("0");
+        const sut = deterministicPartitionKey();
+        expect(sut).toBe("0");
   });
+
+  test("Should return trivial partition key when event has no partitionKey", () => {
+    const event = {};
+    const sut = deterministicPartitionKey(event);
+    expect(sut).toEqual("0");
+  });
+
 
   test("Should return the partition key if it exists", () => {
     const mockEvent = { partitionKey: "abc" };
     const sut = deterministicPartitionKey(mockEvent);
     expect(sut).toBe("abc");
   });
-
-})
+});
